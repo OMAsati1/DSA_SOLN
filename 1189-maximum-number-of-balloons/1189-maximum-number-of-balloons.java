8d9185b2545c39d2b3a70c900ca7e1dff9a103fb
OMAsati1 <omasati73@gmail.com>
2026-06-22 11:38:46 +0530

    Time: 2 ms (96.67%), Space: 43.3 MB (79.23%) - LeetHub - Time: O(n), Space: O(1)

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
@@ -1,30 +1,18 @@
 class Solution {
     public int maxNumberOfBalloons(String text) {
+        int[] cnt = new int[26];
 
-        int[] need = new int[26];
-        need['b' - 'a'] = 1;
-        need['a' - 'a'] = 1;
-        need['l' - 'a'] = 2;
-        need['o' - 'a'] = 2;
-        need['n' - 'a'] = 1;
-
-        int[] freq = new int[26];
-
-        // Count characters in text
-        for (int i = 0; i < text.length(); i++) {
-            freq[text.charAt(i) - 'a']++;
+        for (char c : text.toCharArray()) {
+            cnt[c - 'a']++;
         }
 
-        int ans = Integer.MAX_VALUE;
-
-        // Check only required characters
-        ans = Math.min(ans, freq['b' - 'a'] / need['b' - 'a']);
-        ans = Math.min(ans, freq['a' - 'a'] / need['a' - 'a']);
-        ans = Math.min(ans, freq['l' - 'a'] / need['l' - 'a']);
-        ans = Math.min(ans, freq['o' - 'a'] / need['o' - 'a']);
-        ans = Math.min(ans, freq['n' - 'a'] / need['n' - 'a']);
-
-        return ans;
+        return Math.min(
+                Math.min(cnt['b' - 'a'], cnt['a' - 'a']),
+                Math.min(
+                    Math.min(cnt['l' - 'a'] / 2, cnt['o' - 'a'] / 2),
+                    cnt['n' - 'a']
+                )
+        );
     }
 }
 
