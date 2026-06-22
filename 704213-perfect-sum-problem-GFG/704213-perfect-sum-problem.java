ab01752c09658b8d482c598813a0e59693dfeec8
OMAsati1 <omasati73@gmail.com>
2026-06-22 17:08:56 +0530

    Added solution - LeetHub

--- 704213-perfect-sum-problem-GFG/704213-perfect-sum-problem.java
+++ 704213-perfect-sum-problem-GFG/704213-perfect-sum-problem.java
@@ -3,16 +3,17 @@ class Solution {
     int[][] dp;
 
     public int perfectSum(int[] nums, int target) {
-        int n = nums.length;
+        // int n = nums.length;
 
-        dp = new int[n][target + 1];
+        // dp = new int[n][target + 1];
 
-        // Initialize with -1
-        for (int i = 0; i < n; i++) {
-            Arrays.fill(dp[i], -1);
-        }
+        // // Initialize with -1
+        // for (int i = 0; i < n; i++) {
+        //     Arrays.fill(dp[i], -1);
+        // }
 
-        return solve(0, target, nums);
+        // return solve(0, target, nums);
+        return tab(nums,target);
     }
 
     private int solve(int idx, int target, int[] nums) {
@@ -36,6 +37,32 @@ class Solution {
         }
 
         return dp[idx][target] = take + notTake;
+    }
+    
+    int tab(int[] nums, int target){
+        int n = nums.length;
+
+        int[][] dp = new int[n + 1][target + 1];
+
+        // Base case: idx == n
+        dp[n][0] = 1;
+        // dp[n][j] = 0 for j > 0 (default)
+
+        for (int idx = n - 1; idx >= 0; idx--) {
+            for (int sum = 0; sum <= target; sum++) {
+
+                int notTake = dp[idx + 1][sum];
+
+                int take = 0;
+                if (nums[idx] <= sum)
+                    take = dp[idx + 1][sum - nums[idx]];
+
+                dp[idx][sum] = take + notTake;
+            }
+        }
+
+        return dp[0][target];
+       
     }
 }
 
