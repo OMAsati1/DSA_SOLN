cc2f38c181b9c27d3049b85ab1575c82f119ba1c
OMAsati1 <omasati73@gmail.com>
2026-06-24 10:29:48 +0530

    Added solution - LeetHub

--- 712675-partitions-with-given-difference-GFG/712675-partitions-with-given-difference.java
+++ 712675-partitions-with-given-difference-GFG/712675-partitions-with-given-difference.java
@@ -10,9 +10,30 @@ class Solution {
       return dp[i][target] = solve(nums,i+1,target)+solve(nums,i+1,target-nums[i]);
         
     }
+    
+    int tab(int[] arr, int diff){
+        int sum=0;
+        int n = arr.length;
+        for(int i=0;i<n;i++) sum+=arr[i];
+        if((sum + diff) % 2 != 0) return 0;
+        int tar = (sum+diff)/2;
+        if(diff > sum) return 0;
+        dp=new int[n+1][tar+1];
+        dp[n][0]=1;
+        for(int i=n-1;i>=0;i--){
+            for(int j = 0;j<=tar;j++){
+                int not_take=dp[i+1][j];
+                int take=0;
+                if(j-arr[i]>=0) take = dp[i+1][j-arr[i]];
+                
+                dp[i][j]=take+not_take;
+            }
+        }
+        return dp[0][tar];
+    }
     public int countPartitions(int[] arr, int diff) {
         // code here
-        int sum=0;
+        /*int sum=0;
         int n = arr.length;
         for(int i=0;i<n;i++) sum+=arr[i];
         if((sum + diff) % 2 != 0) return 0;
@@ -20,7 +41,8 @@ class Solution {
         dp=new int[n+1][((sum+diff)/2)+1];
         for(int i = 0; i <= n; i++)
     Arrays.fill(dp[i], -1);
-        return solve(arr,0,(sum+diff)/2);
+        return solve(arr,0,(sum+diff)/2);*/
+        return tab(arr,diff); 
     }
 }
 
