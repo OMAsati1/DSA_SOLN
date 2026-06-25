31ff1b9ee7f55320c53e1e4c76a0fe5f800dd91b
OMAsati1 <omasati73@gmail.com>
2026-06-25 17:24:59 +0530

    Time: 0 ms (100%), Space: 43.4 MB (14.48%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer>inorder=new ArrayList<>();
+        TreeNode cur = root;
+        while(cur!=null){
+            if(cur.left==null){
+                inorder.add(cur.val);
+                cur=cur.right;
+            }
+            else{
+                TreeNode prev = cur.left;
+                while(prev.right!=null && prev.right!=cur){
+                    prev=prev.right;
+                } 
+                if(prev.right==null){
+                    prev.right = cur;
+                    cur=cur.left;
+                }
+                else{
+                    prev.right=null;
+                    inorder.add(cur.val);
+                    cur=cur.right;
+                }
+            }
+        }
+        return inorder;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
