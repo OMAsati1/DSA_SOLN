fb2cff23b8aa55c77b149a6245aca99685adc093
OMAsati1 <omasati73@gmail.com>
2026-06-25 18:03:45 +0530

    Time: 1 ms (17.57%), Space: 44.2 MB (15.41%) - LeetHub - Time: O(n), Space: O(n)

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
@@ -14,14 +14,17 @@
  * }
  */
 class Solution {
-    TreeNode prev=null;
     public void flatten(TreeNode root) {
-      if(root==null) return;
-      flatten(root.right);
-      flatten(root.left);
-      root.right=prev;
-      root.left=null;
-      prev=root;
+        if(root == null) return;
+        Stack<TreeNode> st = new Stack<>();
+        st.push(root);
+        while(!st.isEmpty()){
+            TreeNode cur = st.pop();
+            if(cur.right!=null) st.push(cur.right);
+            if(cur.left!=null) st.push(cur.left);
+            if(!st.isEmpty()) cur.right=st.peek();
+            cur.left=null;
+        }
     }
 }
 
