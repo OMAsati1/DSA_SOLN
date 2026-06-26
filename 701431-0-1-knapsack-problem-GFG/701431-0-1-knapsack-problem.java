0be13dbdb60604f832fc6240d6d0fa7f003279c3
OMAsati1 <omasati73@gmail.com>
2026-06-26 22:30:40 +0530

    Added solution - LeetHub

--- 701431-0-1-knapsack-problem-GFG/701431-0-1-knapsack-problem.java
+++ 701431-0-1-knapsack-problem-GFG/701431-0-1-knapsack-problem.java
@@ -23,12 +23,30 @@ class Solution {
         }
         return dp[0][W];
     }
+    
+    int spcOpt(int W, int val[], int wt[]){
+        int n = val.length;
+        int[] next = new int[W+1];
+        
+        for(int i=n-1;i>=0;i--){
+            int[] cur = new int[W+1];
+            for(int j=0;j<=W;j++){
+               int skip = next[j];
+        int take = Integer.MIN_VALUE;
+        if(j-wt[i]>=0) take = val[i] +next[j-wt[i]]; 
+        cur[j] = Math.max(skip,take);
+            }
+            next = cur;
+        }
+        return next[W];
+    }
     public int knapsack(int W, int val[], int wt[]) {
         // code here
         /*dp = new int[val.length+1][W+1];
         for(int[] row : dp) Arrays.fill(row, -1);
        return rec(0,W,val,wt);*/
-       return tab(W,val,wt);
+    //   return tab(W,val,wt);
+    return spcOpt(W,val,wt);
     }
 }
 
