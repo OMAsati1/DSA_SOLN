c303bc555912153e79d79f1b25c3f44fa1818d69
OMAsati1 <omasati73@gmail.com>
2026-06-26 22:27:41 +0530

    Added solution - LeetHub

--- 701431-0-1-knapsack-problem-GFG/701431-0-1-knapsack-problem.java
+++ 701431-0-1-knapsack-problem-GFG/701431-0-1-knapsack-problem.java
@@ -9,11 +9,26 @@ class Solution {
         return dp[idx][W] = Math.max(skip,take);
         
     }
+    int tab(int W, int val[], int wt[]){
+        int n = val.length;
+        dp = new int[n+1][W+1];
+        
+        for(int i=n-1;i>=0;i--){
+            for(int j=0;j<=W;j++){
+               int skip = dp[i+1][j];
+        int take = Integer.MIN_VALUE;
+        if(j-wt[i]>=0) take = val[i] +dp[i+1][j-wt[i]]; 
+        dp[i][j] = Math.max(skip,take);
+            }
+        }
+        return dp[0][W];
+    }
     public int knapsack(int W, int val[], int wt[]) {
         // code here
-        dp = new int[val.length+1][W+1];
+        /*dp = new int[val.length+1][W+1];
         for(int[] row : dp) Arrays.fill(row, -1);
-       return rec(0,W,val,wt);
+       return rec(0,W,val,wt);*/
+       return tab(W,val,wt);
     }
 }
 
