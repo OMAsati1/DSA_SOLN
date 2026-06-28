2a92a9f18590c21559063cca72c19d72e3d59cb9
OMAsati1 <omasati73@gmail.com>
2026-06-28 18:02:17 +0530

    Added solution - LeetHub

--- 703201-longest-common-substring-GFG/703201-longest-common-substring.java
+++ 703201-longest-common-substring-GFG/703201-longest-common-substring.java
@@ -2,24 +2,24 @@ class Solution {
     public int longCommSubstr(String s1, String s2) {
 
 
-        int n = s1.length();
+       int n = s1.length();
         int m = s2.length();
 
+        int[][] dp = new int[n + 1][m + 1];
+
+        // dp[n][*] = 0 and dp[*][m] = 0
+        // Already initialized to 0 in Java.
         int ans = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 0; j--) {
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-
-                int len = 0;
-
-                while (i + len < n &&
-                       j + len < m &&
-                       s1.charAt(i + len) == s2.charAt(j + len)) {
-
-                    len++;
+                if (s1.charAt(i) == s2.charAt(j)){
+                    dp[i][j] = 1 + dp[i + 1][j + 1];
+                    ans = Math.max(ans,dp[i][j]);
                 }
+                else
+                    dp[i][j] = 0;
 
-                ans = Math.max(ans, len);
             }
         }
 
