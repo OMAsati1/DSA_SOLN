fa56ec48298554abb4ef38df09f8ddb8c30cb7b9
OMAsati1 <omasati73@gmail.com>
2026-07-01 20:31:43 +0530

    Added solution - LeetHub

--- 701750-shortest-common-supersequence-GFG/701750-shortest-common-supersequence.java
+++ 701750-shortest-common-supersequence-GFG/701750-shortest-common-supersequence.java
@@ -41,14 +41,43 @@ class Solution {
         );
     }
     
+    static int tab(String s1, String s2){
+        int n = s1.length();
+        int m = s2.length();
+
+        int[][] dp = new int[n + 1][m + 1];
+
+        // Base cases
+        for (int i = 0; i <= n; i++)
+            dp[i][m] = n - i;
+
+        for (int j = 0; j <= m; j++)
+            dp[n][j] = m - j;
+
+        // Fill table
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 0; j--) {
+
+                if (s1.charAt(i) == s2.charAt(j))
+                    dp[i][j] = 1 + dp[i + 1][j + 1];
+                else
+                    dp[i][j] = 1 + Math.min(dp[i + 1][j], dp[i][j + 1]);
+            }
+        }
+
+        return dp[0][0];
+    }
+    
     public static int minSuperSeq(String s1, String s2) {
         // code here
         // return rec(0, 0, s1, s2);
+        /*
         int n = s1.length(),m = s2.length();
         
         dp = new int[n][m];
         for(var arr : dp) Arrays.fill(arr,-1);
-        return memo(0, 0, s1, s2);
+        return memo(0, 0, s1, s2);*/
+        return tab(s1,s2);
     }
 }
 
