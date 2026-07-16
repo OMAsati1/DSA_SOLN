809204e0af5cf4733d7e47b8d2922ad6b8673a9c
OMAsati1 <omasati73@gmail.com>
2026-07-16 15:45:00 +0530

    Added solution

---
+++ 701135-ceil-in-bst-GFG/701135-ceil-in-bst.java
@@ -0,0 +1,16 @@
+class Solution {
+    int findCeil(Node root, int x) {
+        if (root == null) return -1;
+        
+        if (root.data >= x) {
+            int leftResult = findCeil(root.left, x);
+            return (leftResult != -1) ? leftResult : root.data;
+        } else {
+            return findCeil(root.right, x);
+        }
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
