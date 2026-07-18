be39d721e18758ebf2dd83ce924dab90811f7b28
OMAsati1 <omasati73@gmail.com>
2026-07-18 18:28:44 +0530

    Time: 50 ms (45.94%), Space: 117.7 MB (35.05%) - LeetHub - Time: O(n), Space: O(n)

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -24,14 +24,37 @@ class Solution {
         return result;
     }
 
+    int tab(int[] prices){
+         int n = prices.length;
+        int[][][] dp = new int[n + 1][2][3];
+
+        // base case: dp[n][0][0] = 0, dp[n][1][0] = 0 (already 0 by default in Java)
+
+        for (int i = n - 1; i >= 0; i--) {
+            for(int j=1;j<3;j++){
+            // hold = 0 (nothing held)
+            int buy = -prices[i] + dp[i + 1][1][j];
+            int skip0 = dp[i + 1][0][j];
+            dp[i][0][j] = Math.max(buy, skip0);
+
+            // hold = 1 (holding stock)
+            int sell = prices[i] + dp[i + 1][0][j-1];
+            int skip1 = dp[i + 1][1][j];
+            dp[i][1][j] = Math.max(sell, skip1);
+        }
+        }
+        return dp[0][0][2];
+    }
+
     public int maxProfit(int[] prices) {
-        int n = prices.length;
+        /*int n = prices.length;
         int[][][] memo = new int[n][2][3];
         for (int[][] mat : memo) 
             for(int[] row : mat)
             Arrays.fill(row, -1);
         
-        return rec(prices, 0, 0, memo,2);
+        return rec(prices, 0, 0, memo,2); */
+        return tab(prices);
     }
 }
 
