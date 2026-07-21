a0547279bb73034bb36ecdf6eb8313efb7e05655
OMAsati1 <omasati73@gmail.com>
2026-07-21 16:36:02 +0530

    Time: 72 ms (11.47%), Space: 110.6 MB (85.11%) - LeetHub - Time: O(n), Space: O(k)

---
+++ 1673-find-the-most-competitive-subsequence/1673-find-the-most-competitive-subsequence.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] mostCompetitive(int[] nums, int k) {
+
+        Stack<Integer> st = new Stack<>();
+
+        for (int i = 0; i < nums.length; i++) {
+
+            while (!st.isEmpty()
+                    && st.peek() > nums[i]
+                    && st.size() - 1 + (nums.length - i) >= k) {
+                st.pop();
+            }
+
+            if (st.size() < k)
+                st.push(nums[i]);
+        }
+
+        int[] ans = new int[k];
+
+        for (int i = k - 1; i >= 0; i--)
+            ans[i] = st.pop();
+
+        return ans;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
