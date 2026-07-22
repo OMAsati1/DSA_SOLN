5ef4ba035dbba9f8784da34c6d5583aac5f80155
OMAsati1 <omasati73@gmail.com>
2026-07-22 22:08:24 +0530

    Time: 1 ms (83.72%), Space: 42.9 MB (18.13%) - LeetHub - Time: O(N + M), Space: O(N + M)

---
+++ 0290-word-pattern/0290-word-pattern.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+          String[] words = s.split(" ");
+        if (words.length != pattern.length()) {
+            return false;
+        }
+
+        Map<Character, String> charToWord = new HashMap<>();
+        Map<String, Character> wordToChar = new HashMap<>();
+
+        for (int i = 0; i < pattern.length(); i++) {
+            char c = pattern.charAt(i);
+            String word = words[i];
+
+            if (!charToWord.containsKey(c)) {
+                charToWord.put(c, word);
+            }
+
+            if (!wordToChar.containsKey(word)) {
+                wordToChar.put(word, c);
+            }
+
+            if (!charToWord.get(c).equals(word) || !wordToChar.get(word).equals(c)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
