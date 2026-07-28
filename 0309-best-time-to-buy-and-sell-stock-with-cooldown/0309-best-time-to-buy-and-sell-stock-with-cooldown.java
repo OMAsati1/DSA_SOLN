a2c13f71713527665a57986680722a85e30eea10
OMAsati1 <omasati73@gmail.com>
2026-07-28 14:38:35 +0530

    Time: 1 ms (69.91%), Space: 43.7 MB (32.47%) - LeetHub - Time: O(n), Space: O(n)

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,26 +1,24 @@
 class Solution {
-    int[][] dp;
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        dp = new int[n][3];
-        for(var a : dp) Arrays.fill(a,-1);
-        return rec(prices, 0, 0);
-    }
+        int[][] dp = new int[n + 1][3]; // dp[n][*] = 0 by default
 
-    int rec(int[] prices, int i, int state) {
-        if (i == prices.length) return 0;
-        if(dp[i][state]!=-1) return dp[i][state];
-        if (state == 1) { // hold
-            int sell = prices[i] + rec(prices, i + 1, 2);
-            int skip = rec(prices, i + 1, 1);
-            return dp[i][state] = Math.max(sell, skip);
-        } else if (state == 2) { // coolDown
-            return dp[i][state] = rec(prices, i + 1, 0);
-        } else { // free
-            int buy = -prices[i] + rec(prices, i + 1, 1);
-            int skip = rec(prices, i + 1, 0);
-            return dp[i][state] = Math.max(buy, skip);
+        for (int i = n - 1; i >= 0; i--) {
+            // state 1: hold
+            int sell = prices[i] + dp[i + 1][2];
+            int skip1 = dp[i + 1][1];
+            dp[i][1] = Math.max(sell, skip1);
+
+            // state 2: coolDown
+            dp[i][2] = dp[i + 1][0];
+
+            // state 0: free
+            int buy = -prices[i] + dp[i + 1][1];
+            int skip0 = dp[i + 1][0];
+            dp[i][0] = Math.max(buy, skip0);
         }
+
+        return dp[0][0];
     }
 }
 
