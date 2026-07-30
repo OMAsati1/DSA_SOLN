5e0a612be42aaf7e21cec99b23e8d0d183fe159b
OMAsati1 <omasati73@gmail.com>
2026-07-30 13:54:33 +0530

    Added solution - Time: O(h), Space: O(h)

---
+++ 700614--GFG/700614-.java
@@ -0,0 +1,29 @@
+class Solution {
+    public static ArrayList<Node> findPreSuc(Node root, int key) {
+        ArrayList<Node> ans = new ArrayList<>();
+        ans.add(null); // predecessor
+        ans.add(null); // successor
+        dfs(root, key, ans);
+        return ans;
+    }
+    
+    public static void dfs(Node node, int key, ArrayList<Node> ans){
+        if(node == null) return;
+        
+        if(node.data > key){
+            ans.set(1, node);       // candidate successor
+            dfs(node.left, key, ans);
+        } else if(node.data < key){
+            ans.set(0, node);       // candidate predecessor
+            dfs(node.right, key, ans);
+        } else {
+            // node.data == key, predecessor is max of left subtree, successor is min of right subtree
+            dfs(node.left, key, ans);   // will update predecessor via a helper
+            dfs(node.right, key, ans);  // will update successor via a helper
+        }
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
