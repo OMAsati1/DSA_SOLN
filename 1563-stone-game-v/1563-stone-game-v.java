172e5dfe6c81812f63e2aeb23ad55ae152ac8f40
OMAsati1 <omasati73@gmail.com>
2026-08-21 17:04:28 +0530

    Time: 328 ms (26.57%), Space: 47.8 MB (47.55%) - LeetHub

--- 1563-stone-game-v/1563-stone-game-v.java
+++ 1563-stone-game-v/1563-stone-game-v.java
@@ -1,37 +1,38 @@
 class Solution {
-    int solve(int[] cumSum, int l, int r, int[][] dp){
-        if(r<=l) return 0;
-        if(dp[l][r]!=-1) return dp[l][r];
-
-        int ans = -1;
-        for(int mid=l;mid<r;mid++){
-            int leftSum = cumSum[mid] - (l-1>=0 ? cumSum[l-1] : 0);
-            int rightSum = cumSum[r]-cumSum[mid];
-
-            if(leftSum>rightSum){
-                ans = Math.max(ans,rightSum+solve(cumSum,mid+1,r,dp));
-            }
-            else if(leftSum<rightSum){
-                ans = Math.max(ans,leftSum+solve(cumSum,l,mid,dp));
-            }
-            else{
-                ans = Math.max(ans,Math.max(leftSum+solve(cumSum,l,mid,dp),rightSum+solve(cumSum,mid+1,r,dp)));
-            }
-        }
-        dp[l][r] = ans;
-        return ans;
-    }
-
     public int stoneGameV(int[] stoneValue) {
         int n = stoneValue.length;
         int[] cumSum = new int[n];
-        cumSum[0]=stoneValue[0];
+        cumSum[0] = stoneValue[0];
         for(int i=1;i<n;i++){
-            cumSum[i]=cumSum[i-1]+stoneValue[i];
+            cumSum[i] = cumSum[i-1] + stoneValue[i];
         }
+
         int[][] dp = new int[n][n];
-        for(int[] row : dp) Arrays.fill(row,-1);
-        return solve(cumSum,0,n-1,dp);
+
+        for(int len=1; len<n; len++){
+            for(int l=0; l+len<n; l++){
+                int r = l+len;
+                int ans = -1;
+
+                for(int mid=l; mid<r; mid++){
+                    int leftSum = cumSum[mid] - (l-1>=0 ? cumSum[l-1] : 0);
+                    int rightSum = cumSum[r] - cumSum[mid];
+
+                    if(leftSum>rightSum){
+                        ans = Math.max(ans, rightSum + dp[mid+1][r]);
+                    }
+                    else if(leftSum<rightSum){
+                        ans = Math.max(ans, leftSum + dp[l][mid]);
+                    }
+                    else{
+                        ans = Math.max(ans, Math.max(leftSum + dp[l][mid], rightSum + dp[mid+1][r]));
+                    }
+                }
+                dp[l][r] = ans;
+            }
+        }
+
+        return dp[0][n-1];
     }
 }
 
